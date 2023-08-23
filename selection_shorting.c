eb4d2e0cc44f4480d5e4cdd6915218a5b8954fa7
vishva <hirparajyotsna@gmail.com>
2023-08-23 11:38:04 +0530

    dsa

---
+++ selection_shorting.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void selection(int a[], int n) 
+{  
+  int i, j;  
+  for (i = 0; i < n ; i++) 
+  {  
+    for (j = i+1; j < n; j++)
+
+    
+     {  
+      if (a[i] >a [j])
+       {  
+        int swap = a[j];  
+        a[j] = a[i];  
+        a[i] = swap;  
+      }  
+    }  
+  }  
+}  
+int main()
+ {  
+  int a[] = {42, 34, 25, 12, 22, 11, 90, 9, 5, 42, 24, 35 ,40};  
+  int n = sizeof(a) / sizeof(a[0]);  
+  selection(a, n);  
+
+  printf("Sorted array: ");  
+  for (int i = 0; i < n; i++)
+   {  
+    printf("%d ", a[i]);  
+  }  
+  return 0;
+}
