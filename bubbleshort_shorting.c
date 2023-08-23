1e902e7f16b8b355d7fc940564f1b4f0450d904a
vishva <hirparajyotsna@gmail.com>
2023-08-23 11:23:14 +0530

    dsa

--- bubbleshort_shorting.c
+++ bubbleshort_shorting.c
@@ -2,14 +2,15 @@
 void bubble_sort(int a[], int n) 
 {  
   int i, j;  
-  for (i = 0; i < n - 1; i++) 
+  for (i = 0; i < n ; i++) 
   {  
-    for (j = 0; j < n - i - 1; j++) {  
+    for (j = 0; j < n-1; j++)
+     {  
       if (a[j] >a [j + 1])
        {  
-        int temp = a[j];  
+        int swap = a[j];  
         a[j] = a[j + 1];  
-        a[j + 1] = temp;  
+        a[j + 1] = swap;  
       }  
     }  
   }  
@@ -19,6 +20,7 @@ int main()
   int a[] = {42, 34, 25, 12, 22, 11, 90, 9, 5, 42, 24, 35 ,40};  
   int n = sizeof(a) / sizeof(a[0]);  
   bubble_sort(a, n);  
+
   printf("Sorted array: ");  
   for (int i = 0; i < n; i++)
    {  
